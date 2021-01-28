c07d12e3e7189130fb75bfcf57a78ea624977bf3
Harold Haldemann <harold.haldemann@sfr.fr>
2021-01-28 14:50:15 +0100

    Création de la fonction pour récuperer le code, les prix avec et sans taxe et le nombre disponible

--- singlebook.py
+++ singlebook.py
@@ -10,3 +10,13 @@ def acquire_html(url):
 def acquire_title(soup):
 	title =f"{soup.h1.string}"
 	return title
+
+def acquire_code_tax_number(soup):
+	table = soup.table.find_all('td')
+	code_tax_number = {
+		'universal_product_code': table[0].string,
+		'price_including_tax': table[3].string,
+		'price_excluding_tax': table[2].string,
+		'number_available': table[5].string,
+	}
+	return code_tax_number
