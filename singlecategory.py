45a3148be67e2b69468190ac06d9640a49ba5585
Harold Haldemann <harold.haldemann@sfr.fr>
2021-01-28 16:09:23 +0100

    Création de la fonction récupererant les urls des livres sur toutes les pages d'une catégorie

--- singlecategory.py
+++ singlecategory.py
@@ -2,25 +2,27 @@ from bs4 import BeautifulSoup
 from singlebook import *
 import requests
 
+def acquire_page_list_urls_books(soup):
+	page_list_partial_urls_books = map(
+		lambda x: x.a['href'][8:],
+		soup.find_all('h3'),
+	)
+	page_list_urls_books = map(
+		lambda x: f"http://books.toscrape.com/catalogue{x}",
+		page_list_partial_urls_books,
+	)
+	return list(page_list_urls_books)
+
 def acquire_list_urls_books(url):
-	soup = acquire_html(url)
-	list_partial_urls_books = map(lambda x: x.a['href'][8:], soup.find_all('h3'))
-	list_urls_books = map(lambda x: f"http://books.toscrape.com/catalogue{x}", list_partial_urls_books)
-	return list(list_urls_books)
-
-url = 'http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html'
-print(acquire_list_urls_books(url))
-
-"""
-index = 2
-url = 'http://books.toscrape.com/catalogue/category' + link_category
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-while response.ok:
-	soup_links += soup.find('section').findAll('h3')
-	url = 'http://books.toscrape.com/catalogue/category/books/{0}/page-{1}.html'.format(category, index)
 	response = requests.get(url)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	index += 1
-"""
+	soup = acquire_html(url)
+	list_urls_books = []
+	base_url = url[:-11]
+	index = 2
+	while response.ok:
+		list_urls_books += acquire_page_list_urls_books(soup)
+		next_url = f"{base_url}/page-{index}.html"
+		response = requests.get(next_url)
+		soup = acquire_html(next_url)
+		index += 1
+	return list_urls_books
